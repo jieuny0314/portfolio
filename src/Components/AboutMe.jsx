9cea1adaeb9a33f17d674050e862054e56bcc536
jieuny0314 <ccomo0313@naver.com>
2023-08-28 17:17:06 +0900

    :lipstick: Style : 로고 움직이기

--- src/Components/AboutMe.jsx
+++ src/Components/AboutMe.jsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import Logo from "../Images/Logo_big.png";
 import { useMediaQuery } from "react-responsive";
+import { useState } from "react";
 
 const AboutMeContainer = styled.div`
   width: 100%;
@@ -11,9 +12,31 @@ const AboutMeContainer = styled.div`
   justify-content: center;
   border: 1px solid green;
 
+  .logoImgContainer {
+    width: ${(props) => (props.$ismobile ? "150px" : "350px")};
+    position: relative;
+    height: 125px;
+    background-color: aqua;
+  }
+
   .logoImg {
     width: ${(props) => (props.$ismobile ? "150px" : "350px")};
+    cursor: pointer;
+    position: absolute;
+    top: 0;
+    left: 0;
+    transition: all 1s;
   }
+
+  .logoImgContainer:hover .logoImg {
+    left: -50%;
+    transition: all 1s ease;
+  }
+
+  /* .hover {
+    left: -50%;
+    transition: all 1s ease-in-out;
+  } */
 `;
 
 function AboutMe() {
@@ -21,9 +44,25 @@ function AboutMe() {
     query: "(max-width:767px)",
   });
 
+  const [isHover, setIsHover] = useState(false);
+
+  const handleMouseOver = () => {
+    setIsHover(true);
+  };
+
+  const handleMouseOut = () => {
+    setIsHover(false);
+  };
+
   return (
     <AboutMeContainer $ismobile={isMobile}>
-      <img className="logoImg" src={Logo} alt="LogoImage" />
+      <div
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
+        className="logoImgContainer"
+      >
+        <img className="logoImg" src={Logo} alt="LogoImage" />
+      </div>
     </AboutMeContainer>
   );
 }
