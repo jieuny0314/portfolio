9569d2a462517fe15cc9aca7e66ccaaf65bc84ba
jieuny0314 <ccomo0313@naver.com>
2023-09-05 19:54:31 +0900

    :zap: Feat : 모바일 확대 방지

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -78,8 +78,7 @@ const ProjectsContainer = styled.section`
 const ProjectSliderContainer = styled.div`
   width: ${(props) => (props.$ismobile ? "100%" : "60%")};
   height: ${(props) => (props.$ismobile ? "70%" : "85%")};
-  box-shadow: rgba(0, 0, 0, 0.1) 0px 0px 5px 0px,
-    rgba(0, 0, 0, 0.1) 0px 0px 1px 0px;
+  box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
   display: flex;
   flex-direction: row;
   overflow-x: hidden;
